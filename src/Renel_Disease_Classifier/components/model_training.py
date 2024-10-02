943e99676b2722eeb1abe95cc851a68cf356d6e8
jatin9091@gmail.com <jatin9091@gmail.com>
2024-10-02 06:04:41 +0530

    final commit

--- src/Renel_Disease_Classifier/components/model_training.py
+++ src/Renel_Disease_Classifier/components/model_training.py
@@ -71,7 +71,7 @@ class Training:
             validation_data=self.valid_generator,
         )
         self.save_model(path=self.config.trained_model_path, model=self.model)
-        copy_model(
-            "Renel_Disease_Classification_Deep_Learning/artifacts/training/model.h5",
-            "Renel_Disease_Classification_Deep_Learning/Model",
-        )
+        # copy_model(
+        #     "Renel_Disease_Classification_Deep_Learning/artifacts/training/model.h5",
+        #     "Renel_Disease_Classification_Deep_Learning/Model",
+        # )
